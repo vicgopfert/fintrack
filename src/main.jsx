69ec2e932d3646383fae561520729054b9112dc9
Victor Gopfert <gopfert.web@gmail.com>
2026-08-03 14:23:16 -0300

    chore: add git hooks setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import "./style.css";
+import './style.css';
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App";
+import App from './App';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
