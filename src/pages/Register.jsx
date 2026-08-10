23ba3a60f222eb817bfc31ce9391d0f82a8d4be3
Victor Gopfert <gopfert.web@gmail.com>
2026-08-10 13:04:48 -0300

    feat: add initial register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,47 @@
+import { Link } from 'react-router';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+
 const RegisterPage = () => {
-  return <h1 className="text-2xl font-bold text-sky-600">Register</h1>;
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Crie a sua conta</CardTitle>
+          <CardDescription>
+            Insira suas informações para se registrar.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <Input placeholder="Digite seu nome" />
+          <Input placeholder="Digite seu sobrenome" />
+          <Input placeholder="Digite seu email" />
+          <Input type="password" placeholder="Digite sua senha" />
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full cursor-pointer">Criar conta</Button>
+        </CardFooter>
+      </Card>
+
+      <div className="flex items-center justify-center">
+        <p className="text-sm text-muted-foreground">
+          Já possui uma conta?
+          <Button variant="link" className="cursor-pointer" asChild>
+            <Link to="/login">Faça login</Link>
+          </Button>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
