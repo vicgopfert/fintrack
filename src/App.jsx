ecc1653bc1cf28bcdde66b9e5fc21583b8ae1ea2
Victor Gopfert <gopfert.web@gmail.com>
2026-08-11 12:46:48 -0300

    chore: add sonner setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { Outlet } from 'react-router';
 
+import { Toaster } from '@/components/ui/sonner';
+
 const App = () => {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <Toaster />
+    </>
+  );
 };
 
 export default App;
