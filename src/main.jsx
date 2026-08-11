d85ca3346b29c6917a1c7c7b36c093fab05aa09e
Victor Gopfert <gopfert.web@gmail.com>
2026-08-11 11:19:53 -0300

    chore: add react query setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import './style.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router/dom';
 
 import { router } from '@/routes';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
