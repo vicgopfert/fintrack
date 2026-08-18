4ef921bfdfc447467a564f04ed6541e4900b5a31
Victor Gopfert <gopfert.web@gmail.com>
2026-08-18 16:02:35 -0300

    docs: add js docs to user service

--- src/services/user.js
+++ src/services/user.js
@@ -1,5 +1,40 @@
 import { protectedApi, publicApi } from '@/lib/axios';
 
+/**
+ * @typedef {Object} User
+ * @property {string} id - Identificador do usuário.
+ * @property {string} firstName - Primeiro nome.
+ * @property {string} lastName - Sobrenome.
+ * @property {string} email - E-mail.
+ */
+
+/**
+ * Usuário no formato cru da API, em snake_case.
+ * @typedef {Object} ApiUser
+ * @property {string} id - Identificador do usuário.
+ * @property {string} first_name - Primeiro nome.
+ * @property {string} last_name - Sobrenome.
+ * @property {string} email - E-mail.
+ */
+
+/**
+ * @typedef {Object} Tokens
+ * @property {string} accessToken - Token de acesso, de vida curta.
+ * @property {string} refreshToken - Token usado para renovar o de acesso.
+ */
+
+/**
+ * @typedef {Object} AuthResult
+ * @property {User} user - Usuário autenticado.
+ * @property {Tokens} tokens - Tokens da sessão.
+ */
+
+/**
+ * Converte o usuário da API para o formato usado na aplicação, descartando
+ * os campos não utilizados (incluindo o hash da senha).
+ * @param {ApiUser} user - Usuário como vem da API.
+ * @returns {User} Usuário em camelCase.
+ */
 const mapUser = (user) => ({
   id: user.id,
   firstName: user.first_name,
@@ -8,6 +43,17 @@ const mapUser = (user) => ({
 });
 
 export const UserService = {
+  /**
+   * Cria um novo usuário e já inicia uma sessão para ele.
+   * @param {Object} input - Dados do novo usuário.
+   * @param {string} input.firstName - Primeiro nome.
+   * @param {string} input.lastName - Sobrenome.
+   * @param {string} input.email - E-mail.
+   * @param {string} input.password - Senha, com no mínimo 6 caracteres.
+   * @returns {Promise<AuthResult>} Usuário criado e tokens da sessão.
+   * @throws {import('axios').AxiosError} Se a API rejeitar os dados, por
+   * exemplo quando o e-mail já está cadastrado.
+   */
   register: async (input) => {
     const response = await publicApi.post('/users', {
       first_name: input.firstName,
@@ -22,6 +68,14 @@ export const UserService = {
     };
   },
 
+  /**
+   * Autentica um usuário existente.
+   * @param {Object} input - Credenciais de acesso.
+   * @param {string} input.email - E-mail.
+   * @param {string} input.password - Senha.
+   * @returns {Promise<AuthResult>} Usuário autenticado e tokens da sessão.
+   * @throws {import('axios').AxiosError} Se as credenciais forem inválidas.
+   */
   login: async (input) => {
     const response = await publicApi.post('/auth/login', {
       email: input.email,
@@ -34,6 +88,12 @@ export const UserService = {
     };
   },
 
+  /**
+   * Busca o usuário da sessão atual. O token é anexado pelo interceptor do
+   * `protectedApi`, que também tenta renová-lo em caso de 401.
+   * @returns {Promise<User>} Usuário da sessão.
+   * @throws {import('axios').AxiosError} Se não houver sessão válida.
+   */
   getMe: async () => {
     const response = await protectedApi.get('/users/me');
 
