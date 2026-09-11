7df4b4f8a9823f2a7866d2a77c21c3c4f6215c86
Victor Gopfert <gopfert.web@gmail.com>
2026-09-11 12:21:08 -0300

    feat: add get transactions hook

--- src/api/hooks/use-transactions.js
+++ src/api/hooks/use-transactions.js
@@ -1,8 +1,10 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router';
 import { toast } from 'sonner';
 
 import { mutationKeys, queryKeys } from '@/api/queries/query-keys';
 import { TransactionService } from '@/api/services/transaction';
+import { useAuthContext } from '@/contexts/auth';
 
 export const useCreateTransaction = () => {
   const queryClient = useQueryClient();
@@ -24,5 +26,25 @@ export const useCreateTransaction = () => {
       });
       toast.error('Erro ao adicionar transação. Por favor, tente novamente.');
     },
+  });
+};
+
+export const useGetTransactions = () => {
+  const [searchParams] = useSearchParams();
+  const { user } = useAuthContext();
+
+  const from = searchParams.get('from');
+  const to = searchParams.get('to');
+
+  return useQuery({
+    queryKey: queryKeys.transactions.period(user?.id, from, to),
+
+    queryFn: () => TransactionService.getAll({ from, to }),
+
+    staleTime: 1000 * 60 * 5,
+
+    enabled: !!user?.id && !!from && !!to,
+
+    meta: { errorMessage: 'Erro ao buscar as transações.' },
   });
 };
